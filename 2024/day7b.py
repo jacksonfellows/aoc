9e00a7453a1d0a19449080245ed69941b689fef9
Jackson Fellows <jtfellows@icloud.com>
2024-12-07 10:22:19 -0500

    Be smarter for day 7 part b

--- 2024/day7b.py
+++ 2024/day7b.py
@@ -1,26 +1,28 @@
-import itertools
 import sys
-from operator import add, mul
 
 
-def concat(a, b):
-    t = b
-    n = 0
-    while t > 0:
-        t //= 10
-        n += 1
-    return a*(10**n) + b
+def works(target, args):
+    # "Work backward": idea from post on AOC reddit.
+    if len(args) == 1:
+        return target == args[0]
+    x = args[-1]
+    rem = args[:-1]
+    # +
+    if works(target - x, rem):
+        return True
+    # *
+    if target % x == 0 and works(target // x, rem):
+        return True
+    # ||
+    if str(target).endswith(str(x)) and len(str(target)) > len(str(x)) and works(int(str(target)[:-len(str(x))]), rem):
+        return True
+    return False
 
 S = 0
 for line in sys.stdin.readlines():
     val, rest = line.split(": ")
     val = int(val)
     args = list(map(int, rest.split()))
-    for ops in itertools.product([add, mul, concat], repeat=len(args)-1):
-        res = args[0]
-        for i in range(len(args)-1):
-            res = ops[i](res, args[i+1])
-        if res == val:
-            S += val
-            break
+    if works(val, args):
+        S += val
 print(S)
