9db7cc8c9e88de16bca51843917d488ac5188b14
Jackson Fellows <jtfellows@icloud.com>
2024-12-14 10:37:32 -0500

    Combine day 14 parts a and b (minimize entropy of quad counts)

--- 2024/day14a.py
+++ 2024/day14a.py
@@ -4,16 +4,23 @@ import sys
 import numpy as np
 
 dim = np.array((101, 103))
-quad_counts = np.zeros(4, dtype="int")
+
+ps, vs = [], []
 
 for line in sys.stdin.readlines():
     m = re.search("p=([0-9]+),([0-9]+) v=(-?[0-9]+),(-?[0-9]+)", line)
-    p = np.array((int(m[1]),int(m[2])))
-    v = np.array((int(m[3]),int(m[4])))
-    x,y = (p + 100*v) % dim
-    if x < dim[0]//2 and y < dim[1]//2: quad_counts[0] += 1
-    if x < dim[0]//2 and y > dim[1]//2: quad_counts[1] += 1
-    if x > dim[0]//2 and y > dim[1]//2: quad_counts[2] += 1
-    if x > dim[0]//2 and y < dim[1]//2: quad_counts[3] += 1
+    ps.append(np.array((int(m[1]),int(m[2]))))
+    vs.append(np.array((int(m[3]),int(m[4]))))
 
-print(np.prod(quad_counts))
+def calc_quad_counts(i):
+    q = np.zeros(4, dtype="int")
+    for p, v in zip(ps, vs):
+        x,y = (p + i*v) % dim
+        if x < dim[0]//2 and y < dim[1]//2: q[0] += 1
+        if x < dim[0]//2 and y > dim[1]//2: q[1] += 1
+        if x > dim[0]//2 and y > dim[1]//2: q[2] += 1
+        if x > dim[0]//2 and y < dim[1]//2: q[3] += 1
+    return q
+
+if __name__ == "__main__":
+    print(np.prod(calc_quad_counts(100)))
--- 2024/day14b.py
+++ 2024/day14b.py
@@ -1,32 +1,7 @@
-import re
-import sys
-
-import matplotlib.pyplot as plt
 import numpy as np
 
-dim = np.array((101, 103))
+from day14a import calc_quad_counts
 
-ps, vs = [], []
-
-for line in sys.stdin.readlines():
-    m = re.search("p=([0-9]+),([0-9]+) v=(-?[0-9]+),(-?[0-9]+)", line)
-    ps.append(np.array((int(m[1]),int(m[2]))))
-    vs.append(np.array((int(m[3]),int(m[4]))))
-
-def calc_map(i):
-    map = np.zeros((dim[1],dim[0]))
-    for p, v in zip(ps, vs):
-        x,y = (p + i*v) % dim
-        map[y][x] += 1
-    return map
-
-N = 10_000
-sym_scores = np.zeros(N)
-for i in range(N):
-    map = calc_map(i)
-    sym_scores[i] = np.sum(map*map[:,::-1])
-
-i = sym_scores.argmax()
-plt.title(i)
-plt.imshow(calc_map(i))
-plt.show()
+ii = np.arange(10_000)
+scores = np.vectorize(lambda i: np.prod(calc_quad_counts(i)))(ii)
+print(scores.argmin())
